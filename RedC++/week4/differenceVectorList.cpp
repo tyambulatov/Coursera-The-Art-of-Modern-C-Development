a2e97b098f54b93c97ff6f5f865f0553958e7ef6
tyambulatov <t.yamublatov@gmail.com>
2022-04-01 20:21:50 +0300

    01.04.22

--- RedC++/week4/differenceVectorList.cpp
+++ RedC++/week4/differenceVectorList.cpp
@@ -41,22 +41,15 @@ public:
         data.remove_if(predicate);
     }
 
-    template <typename Predicate>
-    auto FindLast(Predicate predicate)
-    {
-        return find_if(data.rbegin(), data.rend(), predicate);
-    }
-
 private:
     list<int> data;
 };
 
 const int SIZE = 10000;
-const int REMOVAL_COUNT = 1000;
+const int REMOVAL_COUNT = 10000;
 
 int main()
 {
-    /*
     {
         LOG_DURATION("vector");
         NumbersOnVector numbers;
@@ -70,7 +63,6 @@ int main()
                            { return x % REMOVAL_COUNT == i; });
         }
     }
-    */
     {
         LOG_DURATION("list");
         NumbersOnList numbers;
@@ -78,17 +70,10 @@ int main()
         {
             numbers.Add(i);
         }
-        auto it = numbers.FindLast([](int x)
-                                   { return x % REMOVAL_COUNT == 0; });
-        for (int i = 1; i < REMOVAL_COUNT; ++i)
+        for (int i = 0; i < REMOVAL_COUNT; ++i)
         {
             numbers.Remove([i](int x)
                            { return x % REMOVAL_COUNT == i; });
-        }
-        while (*it != 0)
-        {
-            cout << *it << " ";
-            ++it;
         }
     }
     return 0;
